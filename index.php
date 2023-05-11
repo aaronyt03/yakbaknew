df293d5f3134291047e3760e3c370c4c6013f05e
Aaron Lee <aaronyt03@gmail.com>
2023-05-11 19:54:10 +0800

    yakbak initial ui

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YAKBAK 2023</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1>YAK BAK</h1>
+        </header>
+        <div id="yaks">
+            <ul>
+                <li class="yak"><span>5:30PM - </span>Aaron : Hey there!</li>
+                <li class="yak"><span>5:30PM - </span>Aaron : Hey there!</li>
+                <li class="yak"><span>5:30PM - </span>Aaron : Hey there!</li>
+                <li class="yak"><span>5:30PM - </span>Aaron : Hey there!</li>
+                <li class="yak"><span>5:30PM - </span>Aaron : Hey there!</li>
+            </ul>
+        </div>
+        <div id="input">
+            <form method="post" action="process.php">
+            <input type="text" name="user" placeholder="enter your name">
+            <input type="text" name="message" placeholder="type your message">
+            <br>
+            <input type="submit" name="submit" value="yakbak" class="button">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
